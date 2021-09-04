76adf1e553231b0c08dab8af24a761d813f84076
Lasse Jantsch <lassejantsch@web.de>
2021-09-04 19:17:21 +0200

    prepare for merge

--- web/src/components/map.jsx
+++ web/src/components/map.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React, {useState }from 'react'
 import { GoogleMap, LoadScript, Polygon } from '@react-google-maps/api';
 
 import { data } from '../polygons/test';
 
-
 const containerStyle = {
     position: "absolute",
     width: "100%",
-    height: "100%",
+    height: "90%",
 };
 
 const center = {
@@ -15,16 +14,27 @@ const center = {
   lng: 6.790453249906819
 };
 
+const options = {
+  fillColor: "#000",
+  fillOpacity: 0.4,
+  strokeColor: "#000",
+  strokeOpacity: 0.6,
+  strokeWeight: 3
+}
 
-//  const data= [
-//     {lng: 6.79947423091416, lat: 51.2258973117493},
-//     {lng: 6.75569741981624, lat: 51.2560965026146},
-//     {lng: 6.76857032377179, lat: 51.2295152570487},
-//     {lng: 6.79947423091416, lat: 51.2258973117493}
-//   ];
+const options_selected = {
+  fillColor: "#0509ff",
+  fillOpacity: 0.5,
+  strokeColor: "#0509ff",
+  strokeOpacity: 0.7,
+  strokeWeight: 7
+}
 
 
 function Map() {
+
+  const [active, setActive] = useState(false)
+
   return (
     <LoadScript
       googleMapsApiKey="AIzaSyA1PSLKAEjhsNPMpmY72mQnpULpgB0EcEQ"
@@ -32,10 +42,18 @@ function Map() {
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
+        onClick={()=> {
+          setActive(false)
+        }}
         zoom={13}
       >
         <Polygon 
-          path={data}/>
+          path={data}
+          key={1}
+          options= {active? options_selected: options} 
+          onClick={() => {
+            setActive(true)}}
+          />
       </GoogleMap>
     </LoadScript>
   )
