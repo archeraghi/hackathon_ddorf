a7c31f70a0ca06fd16c1ca70c1b82ffde2338ff0
WebDevelopmentDuesseldorf <87103578+WebDevelopmentDuesseldorf@users.noreply.github.com>
2021-09-05 14:29:15 +0200

    Add files via upload
    report form.js

---
+++ web/src/components/ReportForm.js
@@ -0,0 +1,41 @@
+import {useState} from 'react';
+import {database} from '../firebase';
+import { v4 as uuidv4 } from 'uuid';
+  
+function ReportForm() {
+  const [needed, setNeeded] = useState();
+  const [latitude , setLatitude] = useState();
+  const [longitude , setLongitude] = useState();
+      
+  // Push Function
+  const Push = () => {
+    database.ref(uuidv4()).set({
+      needed : needed,
+      latitude : latitude,
+      longitude : longitude
+    }).catch(alert);
+  }
+  
+  return (
+    <div className="App" style={{marginTop : 250}}>
+      <center>
+      <select value={needed} onChange={(e) => setNeeded(e.target.value)}>
+            <option value="food">Food</option>
+            <option value="hygiene">Hygiene Products</option>
+            <option value="medicalSupply">Medical Supply</option>
+            <option value="clothes">Clothes</option>
+      </select>
+      <br/><br/>
+      <input placeholder="latitude" value={latitude} 
+      onChange={(e) => setLatitude(e.target.value)}/>
+      <br/><br/>
+      <input placeholder="longitude" value={longitude} 
+      onChange={(e) => setLongitude(e.target.value)}/>
+      <br/><br/> 
+      <button onClick={Push}>PUSH</button>
+      </center>
+    </div>
+  );
+}
+  
+export default ReportForm;
